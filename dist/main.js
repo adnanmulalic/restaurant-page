21e76602ba90d6998a4b9eb3dad60ee8dc890ecf
adnanmulalic <adnan.mulalic3@gmail.com>
2022-12-14 11:12:13 +0100

    wip

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+alert("this works and changes main.js");
---
+++ src/index.js
@@ -0,0 +1,14 @@
+//import _ from 'lodash';
+
+/* function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, cnow imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component()); */
+
+alert("this works and changes main.js");
