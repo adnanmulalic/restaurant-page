4f92556339f340e4604514b3881559be1191c0b7
adnanmulalic <adnan.mulalic3@gmail.com>
2023-02-09 11:08:22 +0100

    added html elements through createPage function

--- src/firstpage.js
+++ src/firstpage.js
@@ -1,10 +1,22 @@
-const contentDiv = document.getElementById("content");
+import Image from "./images/two-bread-loafs.svg";
 function createPage() {
+    const contentDiv = document.getElementById("content");
     const headerOne = document.createElement("h1");
     headerOne.innerText = "Best Bakery";
+    const breadImage = document.createElement("img");
+    breadImage.src = Image;
+    breadImage.height = "100"; breadImage.width = "200";
+    const textParagraph = document.createElement("p");
+    textParagraph.innerText = "Welcome to the best bakery in town. Our products are made by hand, from scratch, with love. We are the home of good baking.";
+    const textQuote = document.createElement("q");
+    textQuote.innerText = "We bake with love";
     contentDiv.appendChild(headerOne);
+    contentDiv.appendChild(breadImage);
+    contentDiv.appendChild(textParagraph);
+    contentDiv.appendChild(textQuote);
 }
 
+export {createPage};
 //<h1>Best Bakery</h1>
 //<img src="../src/images/two-bread-loafs-svgrepo-com.svg" alt="bread" width="200px" height="100px">
 //<p>Welcome to the best bakery in town. Our products are made by hand, from scratch, with love. We are the home of good baking.</p>
