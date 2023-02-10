cf97d261cd0fd40dc08bed9ce1566957cdb1dcfa
adnanmulalic <adnan.mulalic3@gmail.com>
2023-02-10 13:11:19 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
-import {createPage} from "./firstpage.js";
-createPage();
-//import _ from 'lodash';
+import {createLandingPage} from "./firstpage.js";
+import {createMenuPage} from "./recipes.js";
+import {createContactPage} from "./contact.js";
+import "./style.css";
 
-/* function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, cnow imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component()); */
+const contentDiv = document.getElementById("content");
+contentDiv.classList.add("center");
+
+const homeButton = document.getElementById("home");
+const menuButton = document.getElementById("menu");
+const contactButton = document.getElementById("contact");
+const buttons = document.querySelectorAll("button");
+
+
+createLandingPage();
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    while (contentDiv.firstChild) {
+      contentDiv.removeChild(contentDiv.firstChild);
+    }
+  })
+})
+homeButton.addEventListener("click", createLandingPage);
+menuButton.addEventListener("click", createMenuPage);
+contactButton.addEventListener("click", createContactPage);
+
+
+
+//createLandingPage();
+//createMenuPage();
+//createContactPage();
 
 //alert("this works and changes main.js");
+
+/* switch (button.id) {
+  case "home":
+    createLandingPage;
+    break;
+  case "menu":
+    createMenuPage;
+    break;
+  case "contact":
+    createContactPage;
+    break;
+} */
